2ed9b93e283e2ea5e33f636f8f3279c0fe2fbd45
shadowed-shubh <shadowed.sp@gmail.com>
2026-04-18 05:44:53 +0530

    db

--- Medical_project_datadog_elevenlabs/database.py
+++ Medical_project_datadog_elevenlabs/database.py
@@ -1,9 +1,12 @@
 from sqlalchemy import create_engine, Column, Integer, String, Float, ForeignKey
 from sqlalchemy.orm import declarative_base, sessionmaker, relationship
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-DATABASE_URL = "sqlite:///./auralysis.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
